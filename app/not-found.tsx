d7a87460643b4e605ddf44df052b64cb43e5c98f
mardon1dev <dusbekovmardonbek@gmail.com>
2024-11-28 17:24:35 +0500

    Update

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="mb-4">Sorry, the page you are looking for doesn't exist.</p>
+      <Link href="/">
+        <p className="text-blue-500 underline">Go back to Home</p>
+      </Link>
+    </div>
+  );
+}
